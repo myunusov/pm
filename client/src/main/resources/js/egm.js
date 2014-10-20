2a9b647078df25e60047aa49b4e8bef0c37b8019
Maxim Yunusov <maximyunusov@gmail.com>
2014-10-21 00:11:47 +0700

    DEV: #8 Add editor and calculator for Execute Graph Model

--- client/src/main/resources/js/egm.js
+++ client/src/main/resources/js/egm.js
@@ -13,10 +13,67 @@ function EGMStep(id, name, parent) {
     this.isParent = false;
     this.stepNo = 0;
 
-    this.addStep = function () {
+    this.values = {};
+
+    this.title = function() {
+        return this.id + " " + this.name;
+    };
+
+    this.availableChildren = [
+        {id: "R", title : "Routine"},
+        {id: "L", title : "Loop"},
+        {id: "S", title : "Switch"},
+        {id: "F", title : "Fork"}
+    ];
+
+    this.addStep = function (type) {
         var id2 = this.id + "." + ++this.stepNo;
-        var step = new EGMStep(id2, id2 + " Step", this);
+        var step;
+        switch (type) {
+            case "R":
+                step = new EGMRoutine(id2, this);
+                break;
+            case "L":
+                step = new EGMLoop(id2, this);
+                break;
+            case "S":
+                step = new EGMSwitch(id2, this);
+                break;
+            case "F":
+                step = new EGMFork(id2, this);
+                break;
+            case "C":
+                step = new EGMCase(id2, this);
+                break;
+            default:
+                step = new EGMStep(id2, this);
+        }
         this.steps.push(step);
+    };
+
+    this.change = function(){
+        if (this.parent) {
+            this.parent.resolve();
+        }
+    };
+
+    this.resolve = function(){
+        var result = {};
+        this.steps.each(
+                function(child) {
+                    for (var key in child.values) {
+                        if (child.values.hasOwnProperty(key) && child.values[key]) {
+                            result[key] = result[key] ?
+                                    parseFloat(result[key]) + parseFloat(child.values[key]) :
+                                    child.values[key];
+                        }
+                    }
+                }
+        );
+        this.values = result;
+        if (this.parent) {
+            this.parent.resolve();
+        }
     };
 
     this.removeStep = function (step) {
@@ -43,27 +100,176 @@ function EGMStep(id, name, parent) {
         return !this.isParent() && !this.isRoot();
     };
 
+    this.isLoop = function(){
+        return false;
+    };
+
+    this.isCase = function(){
+        return false;
+    };
+
     this.getClass = function() {
         return this.isParent() ? " parent-li" : "" + " last-child";    // TODO
     }
 
-
 }
 
-function EGMScenario(id, name, model) {
+function EGMScenario(id, model) {
     this.id = id;
-    this.name = name;
+    this.name = "Scenario";
     this.model = model;
     this.parent = null;
     this.steps = [];
     this.stepNo = 0;
+    this.values = {};
 
     this.removeSelf = function () {
         this.model.removeScenario(this);
     };
 
+
 }
 EGMScenario.prototype = new EGMStep();
+
+function EGMRoutine(id, parent, name) {
+    this.id = id;
+    this.name = name || "Step";
+    this.parent = parent;
+    this.steps = [];
+    this.stepNo = 0;
+    this.values = {};
+}
+EGMRoutine.prototype = new EGMStep();
+
+function EGMLoop(id, parent) {
+    this.id = id;
+    this.name = "Loop";
+    this.parent = parent;
+    this.stepNo = 1;
+    this.steps = [];
+    this.stepNo = 0;
+    this.values = {};
+
+    this.iterationNumber = 2;
+
+    this.resolve = function(){
+        var result = {};
+        var iterationNumber = parseFloat(this.iterationNumber);
+        this.steps.each(
+                function(child) {
+                    for (var key in child.values) {
+                        if (child.values.hasOwnProperty(key) && child.values[key]) {
+                            result[key] = result[key] ?
+                                    (parseFloat(result[key]) + parseFloat(child.values[key]) * iterationNumber)  :
+                                    child.values[key] * iterationNumber;
+                        }
+                    }
+                }
+        );
+        this.values = result;
+        if (this.parent) {
+            this.parent.resolve();
+        }
+    };
+
+    this.isLoop = function(){
+        return true;
+    }
+
+}
+EGMLoop.prototype = new EGMStep();
+
+function EGMSwitch(id, parent) {
+    this.id = id;
+    this.name = "Switch";
+    this.parent = parent;
+    this.stepNo = 1;
+    var id2 = this.id + "." + ++this.stepNo;
+    this.steps = [new EGMCase(id2, this)];
+    this.values = {};
+
+    this.availableChildren = [
+        {id: "C", title : "Case"}
+    ]
+}
+EGMSwitch.prototype = new EGMStep();
+
+function EGMCase(id, parent) {
+    this.id = id;
+    this.name = "Case";
+    this.parent = parent;
+    this.stepNo = 0;
+    this.steps = [];
+    this.values = {};
+    this.probability = 1;
+
+    this.resolve = function(){
+        var result = {};
+        var probability = parseFloat(this.probability);
+        this.steps.each(
+                function(child) {
+                    for (var key in child.values) {
+                        if (child.values.hasOwnProperty(key) && child.values[key]) {
+                            result[key] = result[key] ?
+                                    (parseFloat(result[key]) + parseFloat(child.values[key]) * probability)  :
+                            child.values[key] * probability;
+                        }
+                    }
+                }
+        );
+        this.values = result;
+        if (this.parent) {
+            this.parent.resolve();
+        }
+    };
+
+    this.isCase = function(){
+        return true;
+    };
+
+}
+EGMCase.prototype = new EGMStep();
+
+function EGMFork(id, parent) {
+    this.id = id;
+    this.name = "Fork";
+    this.parent = parent;
+    this.stepNo = 0;
+    this.steps = [];
+    this.values = {};
+
+    this.availableChildren = [
+        {id: "R", title : "Routine"},
+        {id: "L", title : "Loop"},
+        {id: "S", title : "Switch"},
+        {id: "F", title : "Fork"}
+    ];
+
+    this.resolve = function(){
+        var result = {};
+        this.steps.each(
+                function(child) {
+                    for (var key in child.values) {
+                        if (child.values.hasOwnProperty(key) && child.values[key]) {
+                            result[key] = result[key] ?
+                                    (
+                                            parseFloat(result[key]) > parseFloat(child.values[key]) ?
+                                                    parseFloat(result[key]) :
+                                                    parseFloat(child.values[key])
+                                    ) :
+                            child.values[key];
+                        }
+                    }
+                }
+        );
+        this.values = result;
+        if (this.parent) {
+            this.parent.resolve();
+        }
+    };
+
+}
+EGMFork.prototype = new EGMStep();
 
 
 function EGM(name, id) {
@@ -102,7 +308,7 @@ function EGM(name, id) {
 
 
     this.addScenario = function () {
-        var scenario = new EGMScenario(++scenarioNo, "Scenario " + scenarioNo, this);
+        var scenario = new EGMScenario(++scenarioNo, this);
         this.scenarios.push(scenario);
     };
 
