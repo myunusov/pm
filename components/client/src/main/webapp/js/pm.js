7208f1f44348f3ce3f7823ae1cd251f7a7053d79
maximyunusov <maximyunusov@gmail.com>
2015-07-14 22:22:07 +0600

    #35 re-design

--- components/client/src/main/webapp/js/pm.js
+++ components/client/src/main/webapp/js/pm.js
@@ -1,18 +1,3 @@
-/*
- * Copyright (c) 2015 Maxim Yunusov
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 angular.module('pm.service', [])
         .service('messageProvider', function () {
             var alerts = [];
@@ -176,7 +161,6 @@ angular.module('pm.filter', [])
 var application = angular.module(
         'pm',
         [
-            'ui.bootstrap',
             'pm.service',
             'pm.directive',
             'pm.filter'
@@ -231,7 +215,7 @@ application.directive('onCollapse', function() {
 });
 
 //noinspection JSUnusedGlobalSymbols
-application.controller('ProjectCtrl', function ($scope, projectProvider, modelFactory)  {
+application.controller('ProjectCtrl', function ($scope, projectProvider, modelFactory, messageProvider)  {
 
     $scope.project = new Project("New Performance Model", uuid());
 
@@ -262,6 +246,8 @@ application.controller('ProjectCtrl', function ($scope, projectProvider, modelFa
     };
 
     $scope.addQNM();
+
+    messageProvider.error("error sample --------------------");
 
 });
 
