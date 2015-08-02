2f4f303131521592dc5078a49d5223c1076062a9
maximyunusov <maximyunusov@gmail.com>
2015-08-02 20:22:43 +0600

    wagon-git commit

---
+++ components/webapp/js/pmc/pmc.fltr.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("pmc.filters",[]).filter("qnmWithout",function(){return function(input,model){var out=[];for(var i=0;i<input.length;i++){if(input[i].type==="qnm"&&input[i]!==model){out.push(input[i])}}return out}});
