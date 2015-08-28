adb14403c1179d5d533385d38f1bac05f3d54919
myunusov <myunusov@luxoft.com>
2015-08-28 18:40:11 +0600

    add Tests Suite

---
+++ components/client/src/test/unit/pmcCtrlSpec.js
@@ -0,0 +1,5 @@
+describe('pmc.ctrl', function() {
+
+
+
+});
---
+++ components/client/src/test/unit/pmcSrvSpec.js
@@ -0,0 +1,22 @@
+describe('pmc.srv', function() {
+
+
+    beforeEach(angular.mock.module("pmc.services"));
+
+    describe("Project Service", function() {
+
+        var projectService;
+
+/*        beforeEach(inject(function (_projectService_) {
+            projectService = _projectService_;
+
+        }));
+
+        it("should do something", function () {
+
+        });*/
+
+    });
+
+
+});
