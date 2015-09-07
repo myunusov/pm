81c698533a36a4db7e6ee37e787350477bc8f10a
myunusov <myunusov@luxoft.com>
2015-09-07 19:00:20 +0600

    jsondoc

--- components/backend/src/main/java/org/maxur/perfmodel/backend/rest/ApplicationResource.java
+++ components/backend/src/main/java/org/maxur/perfmodel/backend/rest/ApplicationResource.java
@@ -3,13 +3,22 @@ package org.maxur.perfmodel.backend.rest;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.jsondoc.core.annotation.Api;
+import org.jsondoc.core.annotation.ApiMethod;
+import org.jsondoc.core.annotation.ApiResponseObject;
+import org.jsondoc.core.annotation.ApiVersion;
+import org.jsondoc.core.pojo.ApiVerb;
 import org.maxur.perfmodel.backend.service.Application;
 import org.maxur.perfmodel.backend.utils.DateTimeUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -24,6 +33,8 @@ import static org.maxur.perfmodel.backend.rest.WebException.badRequestException;
  * @since <pre>11/29/13</pre>
  */
 @Path("/{a:application}")
+@Api(name = "Application Resource", description = "Methods for managing PMC Application")
+@ApiVersion(since = "1.0")
 public class ApplicationResource {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ApplicationResource.class);
@@ -34,7 +45,14 @@ public class ApplicationResource {
     @GET
     @Path("/version")
     @Produces(MediaType.APPLICATION_JSON)
-    public String getVersion() {
+    @ApiMethod(
+        path = "/application/version",
+        verb = ApiVerb.GET,
+        description = "Gets a application version",
+        produces = { MediaType.APPLICATION_JSON },
+        responsestatuscode = "200 - OK"
+    )
+    public @ApiResponseObject String getVersion() {
         return application.version();
     }
 
